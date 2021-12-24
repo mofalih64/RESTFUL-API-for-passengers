a2119f43fddbb16da25590dfa56c8e807a2e4bdb
Mohammed Falih <mohamedfalih98@gmail.com>
2021-12-24 20:09:28 +0300

    Enhancing the CRUD operstions

--- server.js
+++ server.js
@@ -3,37 +3,25 @@ const express = require("express");
 const cityControler = require("./controllers/cityControler");
 const AirportControler = require("./controllers/airportController");
 
-// const db = require("./Database/index");
 
 const app = express();
 
 app.use(express.json());
 app.get("/Cities", cityControler.getAllCities);
-app.post("/addCity", cityControler.addCity);
+app.post("/Cities", cityControler.addCity);
 app.patch("/cities/:id", cityControler.updateCity);
 app.get("/cities/:id", cityControler.getCity_code);
 // app.delete("/cities/:cityName", cityControler.getCity_code);
 
 app.get("/Airports", AirportControler.getAllAirports);
+
+// How to insert the airport data the require city id
 app.post("/Airports", AirportControler.addAirport);
-// app.patch("/Airports/:id", cityControler.updateAirport);
+app.patch("/Airports/:id", AirportControler.updateAirport);
 // app.get("/Airports/:id", cityControler.getCity_code);
-// app.delete("/Airports/:id", cityControler.getCity_code);
+app.delete("/Airports/:id", AirportControler.removeAirport);
 
-// app.post("createFlight", Controlers.addFlight);
-// app.delete("deleteFlight", Controlers.removeFlight);
-// app.patch("updateFlight", Controlers.updateFlight);s
-
-//async (req, res) => {
-//   try {
-//     const theFlights = await db.query("select * from flights");
-//     res.status(200).json({
-//       status: "success",
-//       data: theFlights,
-//     });
-//   } catch (err) {
-//     res.status(404).json(err);
-//   }
+app.post("/signup",userController.addUser);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
